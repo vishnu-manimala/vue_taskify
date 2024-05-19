bc647f5c19120a0e13da68c87ae2f4726ec7fbf2
vishnuManimala <vishnumanimala8133@gmail.com>
2024-05-19 18:43:37 +0530

    list added

---
+++ src/components/TaskList.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-form>
+        <v-container id="taskInput">
+            <v-row>
+                <v-col cols="12">
+                    <v-card class="mx-auto pa-3 ma-2 text-center" max-width="75%">
+                        <v-row>
+
+                            <v-col cols="2">
+
+                                <v-icon>mdi-circle-outline</v-icon>
+
+                            </v-col>
+                            <v-col cols="8">
+                                <v-list-item-title class="headline mb-1"
+                                    id="task">jkhksjbcfkjsdfksdjfsdk</v-list-item-title>
+                            </v-col>
+                            <v-col cols="1">
+
+                                <v-icon color="indigo lighten-4">mdi-star-outline</v-icon>
+
+                            </v-col>
+                            <v-col cols="1">
+
+                                <v-icon color="red lighten-5">mdi-delete</v-icon>
+
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+<script>
+export default {
+    name: "TaskList"
+}
+</script>
+<style scoped>
+#task {
+    display: flex;
+    justify-content: flex-start;
+}
+</style>
--- src/layouts/TodoLayout.vue
+++ src/layouts/TodoLayout.vue
@@ -1,59 +1,61 @@
 <template>
-    <v-card>
-      <v-layout>
-        <v-app-bar
-          color="primary"
-          prominent
-        >
-          <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" id="nav-icon" >Menu</v-app-bar-nav-icon>
-  
-          <v-toolbar-title>TASKIFY</v-toolbar-title>
-  
-          <v-spacer></v-spacer>
-  
-          <template v-if="$vuetify.display.mdAndUp">
-            <v-btn icon="mdi-magnify" variant="text"></v-btn>
-  
-            <v-btn icon="mdi-filter" variant="text"></v-btn>
-          </template>
-  
-          <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
-        </v-app-bar>
-  
-        <SideBarVue  v-model="drawer" />
-  
-        <v-main style="height: 500px; border-radius: 30px;">
-          <v-card-text>
-           <AddTaskVue/>
-          </v-card-text>
-        </v-main>
-      </v-layout>
-    </v-card>
-  </template>
-  <script>
+  <v-card>
+    <v-layout>
+      <v-app-bar color="primary" prominent>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" id="nav-icon"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>TASKIFY</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <template v-if="$vuetify.display.mdAndUp">
+          <v-btn icon="mdi-magnify" variant="text"></v-btn>
+
+          <v-btn icon="mdi-filter" variant="text"></v-btn>
+        </template>
+
+        <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+      </v-app-bar>
+      
+      <!-- side bar -->
+      <SideBarVue v-model="drawer" /> 
+
+      <!-- main -->
+      <v-main style="height: 500px; border-radius: 30px;">
+        <v-card-text>
+          <TaskListVue/>
+          <AddTaskVue />
+        </v-card-text>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+<script>
 import AddTaskVue from '@/components/AddTask.vue'
 import SideBarVue from './SideBar.vue'
+import TaskListVue from '@/components/TaskList.vue'
 
-    export default {
-      name:"TodoLayout",
-      components:{
-        SideBarVue,
-        AddTaskVue
-      } ,
-      data: () => ({
-        drawer: false,
-        group: null,
-      }),
-  
-      watch: {
-        group () {
-          this.drawer = false
-        },
-      },
-    }
-  </script>
-  <style scoped>
-#nav-icon {
-    padding-left: 10px;
+export default {
+  name: "TodoLayout",
+  components: {
+    SideBarVue,
+    AddTaskVue,
+    TaskListVue
+  },
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
 }
-   </style>
+</script>
+<style scoped>
+#nav-icon {
+  padding-left: 10px;
+}
+</style>
